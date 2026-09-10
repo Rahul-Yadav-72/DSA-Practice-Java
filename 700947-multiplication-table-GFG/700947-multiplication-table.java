ec8f4a23a1efa4659e92dc9ff61af90f84a7219e
Rahul Kumar Yadav <ry642499@gmail.com>
2026-09-10 18:16:49 +0530

    Added solution

---
+++ 700947-multiplication-table-GFG/700947-multiplication-table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=1; i<=10; i++){
+            System.out.print((i*n) + " ");
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
