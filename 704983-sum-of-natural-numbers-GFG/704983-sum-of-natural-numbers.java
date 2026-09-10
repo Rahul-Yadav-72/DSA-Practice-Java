39a92e78610b14e5343cc6dd4cef134f47960dde
Rahul Kumar Yadav <ry642499@gmail.com>
2026-09-10 18:19:41 +0530

    Added solution

---
+++ 704983-sum-of-natural-numbers-GFG/704983-sum-of-natural-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        int sum = 0;
+        for(int i=0; i<=n; i++){
+            sum += i;
+        }
+        System.out.print(sum);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
