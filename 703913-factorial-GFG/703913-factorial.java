92892ae9b19664f9d8c2dbff0da2a32233926559
Rahul Kumar Yadav <ry642499@gmail.com>
2026-09-12 09:47:48 +0530

    Added solution find factorial number

---
+++ 703913-factorial-GFG/703913-factorial.java
@@ -0,0 +1,15 @@
+class Solution {
+    int factorial(int n) {
+        // code here
+        int fact = 1;
+        for(int i = 1; i<=n ; i++){
+            fact *= i;
+        }
+        return fact;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
